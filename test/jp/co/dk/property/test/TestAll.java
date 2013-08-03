7a161e6ace0013685c56cb1feeb1328d00eadf29
dk <dk@425babaa-d5dd-46a5-afc5-8c713db7da9f>
2013-08-03 11:48:05 +0000

    ライブラリ構成変更
    git-svn-id: file:///opt/svn/repository/projects/trunk@190 425babaa-d5dd-46a5-afc5-8c713db7da9f

---
+++ test/jp/co/dk/property/test/TestAll.java
@@ -0,0 +1,9 @@
+package jp.co.dk.property.test;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ TestCaseProperties.class, TestProperty.class })
+public class TestAll {}
